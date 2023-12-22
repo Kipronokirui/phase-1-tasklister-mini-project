5114c6010b20f6323ca48e53f17039d77e8e89c2
Kipronokirui <victorkipronokirui2@gmail.com>
2023-12-22 19:03:48 +0300

    Assignemnt done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  let list = document.querySelectorAll('#list');
+  console.log(list);
+
+  let input = document.querySelector('#new-task-description')
+  let taskForm = document.querySelector('#create-task-form');
+
+  console.log(taskForm);
+  taskForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+
+      let list = document.querySelector('#tasks')
+      let listContent = document.createElement('li')
+      listContent.innerText = input.value
+      
+
+      let handleDelete = document.createElement('button')
+      handleDelete.innerText = 'delete'
+
+      list.appendChild(listContent);
+      listContent.appendChild(handleDelete);
+      taskForm.reset()
+
+
+      
+      handleDelete.addEventListener('click', (e) => {
+          listContent.remove();
+
+      })
+
+
+      
+      taskForm.reset()
+  })
+
+
+
 });
+
+
